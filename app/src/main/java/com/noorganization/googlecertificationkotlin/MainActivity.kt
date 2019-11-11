90ff60d486639f22a0a2f6bb8c5c2534ade26664
Carlos N Galves <nicolaugalves@gmail.com>
2019-11-11 19:21:09 -0300

    JobService exemplo

--- app/src/main/java/com/noorganization/googlecertificationkotlin/MainActivity.kt
+++ app/src/main/java/com/noorganization/googlecertificationkotlin/MainActivity.kt
@@ -1,12 +1,98 @@
 package com.noorganization.googlecertificationkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.noorganization.googlecertificationkotlin.codelab_jobscheduler.NotificationJobService
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val JOB_ID = 0
 
 class MainActivity : AppCompatActivity() {
+
+    private var mScheduler: JobScheduler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                if (i > 0) {
+                    seekBarProgress.text = "$i s"
+                } else {
+                    seekBarProgress.text = "Not Set"
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+    }
+
+    fun scheduleJob(view: View) {
+
+        val seekBarInteger = seekBar.progress
+        val seekBarSet = seekBarInteger > 0
+
+        mScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+
+        val selectedNetworkID = networkOptions.checkedRadioButtonId
+        var selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE
+
+        when (selectedNetworkID) {
+            R.id.noNetwork -> selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE
+            R.id.anyNetwork -> selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY
+            R.id.wifiNetwork -> selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED
+        }
+
+        val serviceName = ComponentName(
+            packageName,
+            NotificationJobService::class.java.name
+        )
+
+        val builder =
+            JobInfo
+                .Builder(JOB_ID, serviceName)
+                .setRequiredNetworkType(selectedNetworkOption)
+                .setRequiresDeviceIdle(idleSwitch.isChecked)
+                .setRequiresCharging(chargingSwitch.isChecked)
+
+        if (seekBarSet) {
+            builder.setOverrideDeadline((seekBarInteger * 1000).toLong());
+        }
+
+        val constraintSet = (selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE
+                || idleSwitch.isChecked || chargingSwitch.isChecked) || seekBarSet
+
+        if (constraintSet) {
+            //Schedule the job and notify the user
+            val myJobInfo = builder.build()
+            mScheduler!!.schedule(myJobInfo)
+            Toast.makeText(
+                this,
+                "Job Scheduled, job will run when " + "the constraints are met.",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(
+                this, "Please set at least one constraint",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    fun cancelJobs(view: View) {
+        mScheduler?.let {
+            it.cancelAll()
+            mScheduler = null
+            Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/noorganization/googlecertificationkotlin/codelab_jobscheduler/NotificationJobService.kt
@@ -0,0 +1,75 @@
+package com.noorganization.googlecertificationkotlin.codelab_jobscheduler
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import androidx.core.app.NotificationCompat
+import com.noorganization.googlecertificationkotlin.MainActivity
+import com.noorganization.googlecertificationkotlin.R
+
+class NotificationJobService : JobService() {
+
+    var mNotifyManager: NotificationManager? = null
+
+    // Notification channel ID.
+    private val PRIMARY_CHANNEL_ID = "primary_notification_channel"
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        //Create the notification channel
+        createNotificationChannel()
+
+        //Set up the notification content intent to launch the app when clicked
+        val contentPendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, MainActivity::class.java),
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val builder = NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+            .setContentTitle("Job Service")
+            .setContentText("Your Job ran to completion!")
+            .setContentIntent(contentPendingIntent)
+            .setSmallIcon(R.drawable.ic_job_running)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setAutoCancel(true)
+
+        mNotifyManager!!.notify(0, builder.build())
+        return false
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        return true
+    }
+
+    fun createNotificationChannel() {
+
+        // Define notification manager object.
+        mNotifyManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+            // Create the NotificationChannel with all the parameters.
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID,
+                "Job Service notification",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Notifications from Job Service"
+
+            mNotifyManager!!.createNotificationChannel(notificationChannel)
+        }
+    }
+}
